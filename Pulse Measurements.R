1cc57ed718dd28f1ff8bef95c9e58cf0462f5e16
Chris <64387366+chris91003@users.noreply.github.com>
2022-01-25 14:15:49 -0800

    Delete Pulse Measurements.R

--- Pulse Measurements.R
+++
@@ -1,4 +0,0 @@
-power.t.test(n= NULL, delta= 8, sd= 5, sig.level= 0.05,power= 0.8, type="two.sample")
-# use boxplot to make comparisons of means              
-#regress pulse against sex and maybe collect other information (weight, height, age)             
-             
